2987dac40ba34bc297474418e7b41d3dd801117f
Carolina Fernandez <carolina.fernandez@i2cat.net>
2019-12-13 07:55:22 -0800

    6-refactor-dataplane: simplify & beautify Python tests

--- dataplane/bmodel/efcp_router/bm_tests/tests/efcp_test.py
+++ dataplane/bmodel/efcp_router/bm_tests/tests/efcp_test.py
@@ -2,136 +2,83 @@
 nplsim test script
 """
 
+from nplclass_base import EFCP
+from npltest_base import NPLBaseTest
+from npltest_base import npl_test_run
 from scapy.all import *
-import os
-import sys
 
-BATCH_TEST_DIR = os.path.join(os.path.dirname(__file__), os.environ['NCS_ROOT'] + '/bmi/cli/bti')
-sys.path.append(BATCH_TEST_DIR)
-from batch_test_base import BatchTestBase, socket_port_get, reg_file_get
 
-CFG_FILE = 'bm_tests/l2_test/tbl_cfg.txt'
+# Packet configuration
+mac_dst_addr = "00:00:05:00:00:02"
+mac_src_addr = "00:00:05:00:00:01"
 
-#Pkt config
-dst_mac = "00:00:05:00:00:02"
-src_mac = "00:00:05:00:00:01"
-class EFCP(Packet):
-    name = "EFCP"
-
-    # XByteField --> 1 Byte-integer (X bc the representation of the fields
-    #                               value is in hexadecimal)
-    #ShortField --> 2 Byte-integer
-    #IntEnumField --> 4 Byte-integer
-
-    fields_desc = [ XByteField("version", 0x01),
-                    ShortField("dst_addr", 0x00),
-                    ShortField("scr_addr", 1),
-                    XByteField("qos_id", 0x00),
-                    ShortField("dst_cepid", 0),
-                    ShortField("src_cepid", 0),
-                    XByteField("pdutype", 0x00),
-                    XByteField("flags", 0x00),
-                    ShortField("length", 1),
-                    IntField("seq_num", 0),
-                    XShortField("chksum", 0x00)]
-
+# ScaPy initialisation
 bind_layers(Ether, EFCP, type=0xD1F)
 
-def get_tx_packet(test_num):
-    pkt = Ether() / EFCP(dst_addr=2, pdutype=0x80)
-    pkt[Ether].dst = dst_mac
-    pkt[Ether].src = src_mac
-    pkt = pkt/"This EFCP packet #{} from CLI to BM :)".format(test_num)
-    return pkt
 
-class Test(object):
-    """ test class
+class Test(NPLBaseTest):
+    """
+    Test class
         connect to nplsim, run test, disconnect from nplsim
     """
-    bt_if = None
-    connected = False
-    batch_mode = True
 
-    def __init__(self, batch_mode, host, sport, reg_file, cfg_file):
-        self.batch_mode = batch_mode
-        self.bt_if = BatchTestBase()
+    def __get_tx_packet(self, test_num, ipc_dst_addr):
+        pkt = Ether() / EFCP(ipc_dst_addr=ipc_dst_addr, pdutype=0x80)
+        pkt[Ether].dst = mac_dst_addr
+        pkt[Ether].src = mac_src_addr
+        pkt = pkt/"EFCP packet #{} sent from CLI to BM :)".format(test_num)
+        return pkt
 
-        self.connected = self.bt_if.connect(host, sport, reg_file)
-
-        if self.batch_mode:
-            # if batch_mode then load config file
-            stat = self.bt_if.configure(cfg_file)
-
-            (stat, data) = self.bt_if.process_txt_cmd('debug_level 4')
-    
-
-    def run(self):
-        """ run test
+    # IMPORTANT: all tests must follow the pattern "test_*" in their names
+    def test_pkt_transmitted_is_received(self):
         """
-        # port list to send packets
-        port = [1]
+        Verify that an EFCP packet being transmitted to port X is received in this same port
+        """
+        # list of ports to send packets to
+        ports = [1]
 
-        # number of packets to send
+        # destination IPC address
+        ipc_dst_addr = 2
+
+        # number of packets to be sent
         numpkts = 1
 
         for count in range(numpkts):
-
-            for i in port:
-
+            for port in ports:
                 # get packet
-                
-                pkt = get_tx_packet(i+1)
+                tx_pkt = self.__get_tx_packet(port+1, ipc_dst_addr)
 
-
-                print "##################################"
-                print "TX PKT num {} on port {}:".format(count, i)
-                hexdump(pkt)
+                print("--- Submitting packet(s)")
+                print("TX PKT num #{} to port {}:".format(count, port))
+                hexdump(tx_pkt)
 
                 # transmit packet
-                recv_pkts = self.bt_if.send_pkt(i, pkt, len(str(pkt)))
+                recv_pkts = self.bt_if.send_pkt(port, tx_pkt, len(str(tx_pkt)))
 
+                print("--- Receiving packet(s)")
                 for rx_pkt in recv_pkts:
                     # check tx status
-                    if rx_pkt['STATUS'] < 0:
-                        print "TX ERROR"
+                    if rx_pkt["STATUS"] < 0:
+                        print("TX ERROR")
                         continue
 
                     # check receive packet status
-                    if rx_pkt['STATUS']:
-                        print "Packet is Dropped!"
+                    if rx_pkt["STATUS"]:
+                        print("Packet is dropped!")
                         continue
 
-                    print "RX PKT on port {}:".format(rx_pkt['PORT'])
-                    hexdump(rx_pkt['PACKET'])
+                    print("RX PKT in port {}:".format(rx_pkt["PORT"]))
+                    hexdump(rx_pkt["PACKET"])
+
+                    # Verify that the payload of each sent packet is present at each received packet
+                    # - Note: not use of equals but a substring in the emitted packet due to the presence of multiple
+                    # - non-ASCII symbols in the received payload and because this may be incomplete as well
+                    efcp_payload_excerpt = str(tx_pkt.getlayer("EFCP").payload)[:20]
+                    self.assertTrue(efcp_payload_excerpt in str(rx_pkt["PACKET"]))
+                    # Verify that the port where the packet was submitted to is correct
+                    self.assertTrue(ipc_dst_addr == tx_pkt.getlayer("EFCP").fields["ipc_dst_addr"])
 
 
-    def exit(self):
-        """ disconnect from nplsim
-        """
-        if self.batch_mode:
-            # if batch_mode then close nplsim,
-            self.bt_if.exit()
-
-if __name__ == '__main__':
-    """ test.py -port <socket port num> -fe <output dir>
-        if -fe is specified then running in batch_mode:
-            configure nplsim tables
-            terminate nplsim process
-    """
-    # get socket port
-    socket_port = socket_port_get(sys.argv)
-
-    # get register file
-    reg_file = reg_file_get(sys.argv)
-    batch_mode = True
-    if reg_file == None:
-        batch_mode = False
-
-    # connect to nplsim
-    test_if = Test(batch_mode, 'localhost', socket_port, reg_file, CFG_FILE)
-
-    # run tests
-    test_if.run()
-
-    # close nplsim
-    test_if.exit()            
+if __name__ == "__main__":
+    # IMPORTANT: do not change this line
+    npl_test_run(__name__, Test)
