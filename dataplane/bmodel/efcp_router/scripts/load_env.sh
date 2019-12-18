3bca02213c7a747ad6fcc412cf111fb8700b4486
Carolina Fernandez <carolina.fernandez@i2cat.net>
2019-12-18 12:53:22 -0800

    9-test-enhancement: absolute path for integration vm

--- dataplane/bmodel/efcp_router/scripts/load_env.sh
+++ dataplane/bmodel/efcp_router/scripts/load_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #sudo bash
-cd ~/ncsc-1.3.3rc4
+cd /home/npl/ncsc-1.3.3rc4
 source ./bin/setup.sh
 export NPL_EXAMPLES=/home/npl/rina_router/dataplane/bmodel
 cd $NPL_EXAMPLES/efcp_router
