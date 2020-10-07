73acce1c58733dea3c2931bb96a5a480875a09f2
Carolina Fernandez <carolina.fernande@i2cat.net>
2020-10-07 04:48:43 -0700

    28-port-interior-router-dataplane-from-bmv2-to-the-tofino-model: return code, add checks

--- controlplane/p4/src/server/docker-entrypoint.sh
+++ controlplane/p4/src/server/docker-entrypoint.sh
@@ -3,20 +3,25 @@
 ready_signal=/tmp/.stratum_bmv2.compiled
 [[ -f ${ready_signal} ]] && rm -f ${ready_signal}
 
+#[[ -d ~/.cache/bazel ]] && sudo chown -R $(whoami):$(whoami) ~/.cache/bazel/
+ls -lah ~/.cache
+mkdir -p ~/.cache/bazel
+[[ -d ~/.cache/bazel ]] && sudo chown -R $(whoami):$(whoami) ~/.cache/bazel/
+
+sudo chown -R $(whoami):$(whoami) /stratum/stratum/hal/lib/common/
 # Copy class with debug options for logic being served
 cp -p /p4_service.cc /stratum/stratum/hal/lib/common/
+sudo chown -R $(whoami):$(whoami) /stratum/stratum/hal/lib/bmv2/
 # Copy class with debug options for bmv2 handler
 cp -p /bmv2_switch.cc /stratum/stratum/hal/lib/bmv2/
 
 # Fix for internal cfg error
 sudo mkdir -p /etc/stratum
-sudo chown -R $(whoami):$(whoami) /etc/stratum -R
+sudo chown -R $(whoami):$(whoami) /etc/stratum/
 
 # Build code
-bazel build //stratum/hal/lib/common/...
-
-# Copy chassis configuration for bmv2 model
-cp -p /stratum/stratum/hal/bin/bmv2/dummy.json /stratum/bazel-bin/stratum/hal/bin/bmv2/
+sudo bazel build //stratum/hal/lib/common/...
+echo "Result of first bazel build: $?"
 
 # Copy changes to external libraries
 #cp -p /deps.bzl /stratum/bazel/
@@ -30,12 +35,27 @@ else
 fi
 
 # Build code
-bazel build //stratum/hal/bin/bmv2:stratum_bmv2
+#[[ -d ~/.cache/bazel ]] && sudo chown -R $(whoami):$(whoami) ~/.cache/bazel/
+sudo mkdir -p ~/.cache/bazel/_bazel_gitlab-runner
+ls -lah ~/.cache/bazel/_bazel_gitlab-runner
+sudo chown $(whoami):$(whoami) -R ~/.cache
+#sudo chown $(whoami):$(whoami) -R /tmp
 
+bazel build //stratum/hal/bin/bmv2:stratum_bmv2
+echo "Result of second bazel build: $?"
+
+# Copy chassis configuration for bmv2 model
+sudo chown -R $(whoami):$(whoami) /stratum/bazel-bin/stratum/hal/bin/bmv2/
+cp -p /stratum/stratum/hal/bin/bmv2/dummy.json /stratum/bazel-bin/stratum/hal/bin/bmv2/
+
+sudo chown -R $(whoami):$(whoami) bazel-bin/stratum/hal/bin/bmv2
 cd bazel-bin/stratum/hal/bin/bmv2
 
 # Indicate server is properly built/generated
-touch ${ready_signal}
+sudo touch ${ready_signal}
+sudo chown -R $(whoami):$(whoami) ${ready_signal}
 
 # Run server
 ./stratum_bmv2 --initial-pipeline=dummy.json
+
+echo 0
